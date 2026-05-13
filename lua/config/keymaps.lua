0ae09e1e063daecc4c62ec406cc55a73feeb2ac1
g0at1 <michal.lendzion@vp.pl>
2026-05-13 18:07:48 +0200

    Tmux plugin and new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>r", ":RunCode<CR>")
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<leader>x", ":bdelete<CR>")
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "move left" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "move right" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "move down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "move up" })
 
 local builtin = require("telescope.builtin")
 
